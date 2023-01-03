7223cee07873f36d2685aa28de58bb995b2b7738
Raf-main <rafikrrrafik@gmail.com>
2023-01-03 19:10:27 +0300

    added base controller with IMediator and IMapper

---
+++ src/BuildingBlocks/ProjectManagement.API.Shared/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.API.Shared.Controllers;
+
+public abstract class BaseController : ControllerBase
+{
+    protected readonly IMapper Mapper;
+    protected readonly IMediator Mediator;
+
+    protected BaseController(IMapper mapper, IMediator mediator)
+    {
+        Mapper = mapper;
+        Mediator = mediator;
+    }
+}
